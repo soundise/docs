e8856f78bc830dcbc2cb3aea5672daf59f7d931b
olekbaran <olekbaran24@gmail.com>
2023-02-11 14:07:40 +0100

    feat: styled components theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,34 @@
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme } from '@soundise/react-components';
+import { darkTheme, lightTheme } from '@soundise/react-components';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider theme={darkTheme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  useEffect(() => {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'class') {
+          setTheme(element.classList.contains('dark') ? 'dark' : 'light');
+        }
+      });
+    });
+
+    const element = document.documentElement;
+    setTheme(element.classList.contains('dark') ? 'dark' : 'light');
+    observer.observe(element, { attributes: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
