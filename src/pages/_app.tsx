accc8bda7c8c665857c61b888bf36d07c42ea3ef
Olek Baran <olekbaran24@gmail.com>
2023-02-12 10:19:38 +0100

    feat: useDarkMode hook (#4)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,10 @@
 import type { AppProps } from 'next/app';
-import { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from '@soundise/react-components';
+import { useDarkMode } from 'hooks';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'class') {
-          setIsDarkMode(element.classList.contains('dark'));
-        }
-      });
-    });
-
-    const element = document.documentElement;
-    setIsDarkMode(element.classList.contains('dark'));
-    observer.observe(element, { attributes: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
+  const isDarkMode = useDarkMode();
 
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
